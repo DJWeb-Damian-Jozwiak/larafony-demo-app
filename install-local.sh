cfe4a70e845b0d97c4a4121d36e9abb22900e71a
Damian Jóźwiak <damian.jozwiak.lodz@gmail.com>
2025-10-26 18:56:14 +0100

    demo app for frameork v2.0.0-RC1.2

--- install-local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /var/www/projekty/book/demo-app
-echo "Installing with local framework symlink..."
-rm -rf vendor composer.lock
-php8.5 /usr/local/bin/composer config repositories.local '{"type": "path", "url": "../framework", "options": {"symlink": true}}'
-php8.5 /usr/local/bin/composer install
-ls -la vendor/larafony/core
-echo "Done!"
